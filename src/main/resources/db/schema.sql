5ec54416ec1b24ec2d7262a2a8eed2adabd72e32
Shadi <sh.moonwalker7@gmail.com>
2026-06-16 15:07:19 +0200

    Add initial database schema with tables and relationships

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,51 @@
+CREATE TYPE ticket_status AS ENUM (
+    'OPEN', 'IN PROGRESS', 'CLOSED');
+
+CREATE TABLE "user" (
+    id bigserial primary key ,
+    name varchar(255) not null CHECK (char_length(name) >= 3),
+    email varchar(255) not null unique
+
+);
+CREATE TABLE project (
+    id bigserial primary key ,
+    name varchar(255) not null,CHECK (char_length(name) >= 3)
+);
+CREATE TABLE ticket (
+    id bigserial primary key ,
+    title varchar(255) not null,
+    description varchar(255) ,
+    project_id BIGINT NOT NULL,
+    status ticket_status not null,
+    creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date TIMESTAMP,
+    CONSTRAINT fk_ticket_project
+        FOREIGN KEY (project_id)
+            REFERENCES project(id)
+
+);
+
+CREATE TABLE user_ticket (
+    user_id BIGINT NOT NULL,
+    ticket_id BIGINT NOT NULL,
+
+   primary key (user_id, ticket_id),
+
+    CONSTRAINT fk_user_ticket_user
+        FOREIGN KEY (user_id)
+            REFERENCES "user"(id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_user_ticket_ticket
+        FOREIGN KEY (ticket_id)
+            REFERENCES ticket(id)
+            ON DELETE CASCADE
+    );
+
+CREATE INDEX idx_tickets_status
+    ON ticket(status);
+
+CREATE INDEX idx_tickets_project_id
+    ON ticket(project_id);
+
+CREATE INDEX idx_user_ticket_user_id
+    ON user_ticket(user_id);
